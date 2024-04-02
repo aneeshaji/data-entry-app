ff5adc034a0b67cbc8dcca8d57c75ce88f7ecd75
Aneesh Ajithkumar <aaneeshaji@gmail.com>
2024-04-02 19:40:36 +0530

    JQRMS-19

---
+++ database/migrations/2024_04_02_140041_create_general_information_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('general_information', function (Blueprint $table) {
+            $table->id();
+            $table->integer('basic_details_id')->comment('Refers basic_details -> id');
+            $table->string('job_number')->nullable();
+            $table->string('customer')->nullable();
+            $table->string('location')->nullable();
+            $table->string('end_user')->nullable();
+            $table->string('engineer')->nullable();
+            $table->string('designer')->nullable();
+            $table->string('sales_man')->nullable();
+            $table->string('job_type')->nullable();
+            $table->string('job_revision_number')->nullable();
+            $table->date('date_revised')->nullable();
+            $table->date('date_approved')->nullable();
+            $table->text('notes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('general_information');
+    }
+};
