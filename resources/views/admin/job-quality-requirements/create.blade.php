a5736f72ae421b79c242e662d5ba150b8ccb326d
Aneesh Ajithkumar <aaneeshaji@gmail.com>
2024-08-28 12:35:15 +0530

    JQRMS-51

--- resources/views/admin/job-quality-requirements/create.blade.php
+++ resources/views/admin/job-quality-requirements/create.blade.php
@@ -3,7 +3,7 @@
         <!-- Content -->
         <div class="container-xxl flex-grow-1 container-p-y">
             <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Job Quality Requirements / </span> 
-                {{ isset($jqr->id) ? "Edit - " . $jqr->job_name . "" : "Create" }}
+                {{ isset($jqr->id) ? "Edit - " . $jqr->job_number . "" : "Create" }}
             </h4>
             <!-- <p class="mb-4">
                 Icons used on this page are made by
