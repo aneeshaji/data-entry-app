5c06ca348f076564c0a40f2fed25a63a83a7df87
Aneesh Ajithkumar <aaneeshaji@gmail.com>
2024-09-02 22:35:10 +0530

    JQRMS-57

---
+++ database/migrations/2024_09_02_113321_update_non_code_vessels_tanks_add_doc_status_fields.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('non_code_vessels_tanks', function (Blueprint $table) {
+            $table->string('status_of_docs_deliverables_nde')->after('nde_reports_comments')->nullable();
+            $table->string('status_of_docs_deliverables_hydro')->after('status_of_docs_deliverables_nde')->nullable();
+            $table->string('status_of_docs_deliverables_heat_map')->after('status_of_docs_deliverables_hydro')->nullable();
+            $table->string('status_of_docs_deliverables_weld_map')->after('status_of_docs_deliverables_heat_map')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('non_code_vessels_tanks', function (Blueprint $table) {
+            //
+        });
+    }
+};
